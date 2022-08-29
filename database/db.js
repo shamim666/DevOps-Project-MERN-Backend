d3ec28e393019a2b90d9b62a010a6e3c4b5793b6
shamim <shamim.pstu@gmail.com>
2022-08-29 17:39:26 +0600

    sms backend

---
+++ database/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //db: 'mongodb://localhost:27017/reactdb'
+  //db: 'mongodb://smsmern:JziehBT1OEoogwKlFpQzUy0A2O8V4Gsiu8onhAagmbLxpGKYgorRglrlnWjErCkDGEZGJndMEc7P6DM38cfJZQ==@smsmern.mongo.cosmos.azure.com:10255/sms-mern?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@smsmern@'
+  db: 'mongodb://mernregister:3Q4SWZx49udpDwKMmKQI9DANnKhJI0nc4JNI6RY44S3UyAG4Kd2asbfP0nzoL854SU2hMqH4R3FeBpellLvTzw==@mernregister.mongo.cosmos.azure.com:10255/registerapp?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@mernregister@'
+};
